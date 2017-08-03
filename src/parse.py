011f56811f5f867bd01e61ac709ed1f8f2639b17
Samson Endale <4sam21@gmail.com>
2017-08-03 17:45:36 +0300

    probably done by now

--- src/parse.py
+++ src/parse.py
@@ -1,5 +1,6 @@
+import os
 import re
-from pprint import pprint
+import sys
 
 KEYWORD_VAR = '(?:var)'
 KEYWORD_IF = '(?:if)'
@@ -11,7 +12,7 @@ KEYWORD_ELSE = '(?:else)'
 KEYWORD_WRITE = '(?:write)'
 KEYWORD_READ = '(?:read)'
 
-NUMBER_LITERAL = '([-]?[1-9]+[0-9]*)'
+NUMBER_LITERAL = '(?:([-]?[1-9]+[0-9]*)|(0))'
 VARIABLE_IDENTIFIER = '([A-Za-z_]+[A-Za-z_0-9]*)'
 STRING_LITERAL = '("[^"]*")'
 
@@ -23,21 +24,23 @@ OP_LOGICAL = "([><~])"
 EXPRESSION = "(?:(?:%s$)|(?:(?:%s)\s*(?:%s)\s*(?:%s)))" % (VALUE, VALUE, OP_ARITHMETIC, VALUE)
 CONDITION = "(?:(?:%s)\s*(?:%s)\s*(?:%s))" % (VALUE, OP_LOGICAL, VALUE)
 
-ASSIGNMENT_STATEMENT = "(?:(?:%s)\s*=\s*(?:%s))" % (VARIABLE_IDENTIFIER, EXPRESSION)
+ASSIGNMENT_STATEMENT = "^(?:(?:%s)\s*=\s*(?:%s))$" % (VARIABLE_IDENTIFIER, EXPRESSION)
 
-VAR_DECLARE_STATEMENT = "(?:%s\s*(?:%s)\s*(?:=\s*(?:%s))?)" % (KEYWORD_VAR, VARIABLE_IDENTIFIER, EXPRESSION)
+VAR_DECLARE_STATEMENT = "^(?:%s\s*(?:%s)\s*(?:=\s*(?:%s))?)$" % (KEYWORD_VAR, VARIABLE_IDENTIFIER, EXPRESSION)
 
-IF_CONDITION = "^(?:%s\s*(?:%s)\s*(?:%s))$" % (KEYWORD_IF, CONDITION, KEYWORD_DO)
-ELIF_CONDITION = "^(?:%s\s*(?:%s)\s*(?:%s))$" % (KEYWORD_ELIF, CONDITION, KEYWORD_DO)
+_C_ = "^(?:%s\s*(?:%s)\s*(?:%s))$"
+
+IF_CONDITION = _C_ % (KEYWORD_IF, CONDITION, KEYWORD_DO)
+ELIF_CONDITION = _C_ % (KEYWORD_ELIF, CONDITION, KEYWORD_DO)
 ELSE_CONDITION = "^(?:%s\s*(?:%s))$" % (KEYWORD_ELSE, KEYWORD_DO)
 END_CONDITION = "^(?:%s)$" % KEYWORD_END
 
-WHILE_CONDITION = "^(?:%s\s*(?:%s)\s*(?:%s))$" % (KEYWORD_WHILE, CONDITION, KEYWORD_DO)
+WHILE_CONDITION = _C_ % (KEYWORD_WHILE, CONDITION, KEYWORD_DO)
 
-WRITE_STATEMENT = "(?:%s\s*(?:(?:%s)|(?:%s)))" % (KEYWORD_WRITE, EXPRESSION, STRING_LITERAL)
-READ_STATEMENT = "(?:%s\s*(?:%s))" % (KEYWORD_READ, VARIABLE_IDENTIFIER,)
-COMMENT = "[#].*"
+WRITE_STATEMENT = "^(?:%s\s*(?:(?:%s)|(?:%s)))$" % (KEYWORD_WRITE, EXPRESSION, STRING_LITERAL)
+READ_STATEMENT = "^(?:%s\s*(?:%s))$" % (KEYWORD_READ, VARIABLE_IDENTIFIER,)
 
+COMMENT = "^[#](.*)$"
 rules = [
     ('var', VAR_DECLARE_STATEMENT),
     ('ass', ASSIGNMENT_STATEMENT),
@@ -45,11 +48,57 @@ rules = [
     ('elif', ELIF_CONDITION),
     ('else', ELSE_CONDITION),
     ('end', END_CONDITION),
+    ('while', WHILE_CONDITION),
     ('write', WRITE_STATEMENT),
     ('read', READ_STATEMENT),
     ('com', COMMENT),
 ]
 
+
+def generator(tokens):
+    code = '#include <stdio.h>\n\nint main() {\n\t'
+
+    for n, t in tokens:
+        if n == 'var':
+            code += 'int '
+            code += t[0]
+            if len(t) > 1:
+                code += ' = '
+                code += ' '.join(t[1:])
+            code += ';'
+        elif n == 'ass':
+            code += t[0]
+            if len(t) > 1:
+                code += ' '.join(t[1:])
+            code += ';'
+        elif n == 'ass':
+            code += t[0]
+            declared_vars.append(t[0])
+            if len(t) > 1:
+                code += ' '.join(t[1:])
+            code += ';'
+        elif n == 'read':
+            code += 'scanf("%d", &' + t[0] + ' );'
+        elif n == 'write':
+            if t[0].startswith('"'):
+                code += 'printf("%s\\n", ' + t[0] + ' );'
+            else:
+                code += 'printf("%d\\n", ' + t[0] + ' );'
+        elif n == 'while':
+            code += 'while (' + (' '.join(t).replace('~', '==')) + ') { '
+        elif n == 'if':
+            code += 'if (' + (' '.join(t).replace('~', '==')) + ') { '
+        elif n == 'elif':
+            code += '} else if (' + (' '.join(t).replace('~', '==')) + ') { '
+        elif n == 'else':
+            code += '} else { '
+        elif n == 'end':
+            code += '}'
+        else:
+            pass
+        code += '\n\t'
+
+    return code + '\n}'
 
 def parse(line):
     for name, rule in rules:
@@ -58,16 +107,25 @@ def parse(line):
             tokens = filter(None, mc.groups())
             return name, tokens
 
+    sys.stderr.write('Error on line "' + line + '"')
+    exit(1)
+
 
 def read():
     content = open('../sample/code.418', 'r').read()
 
-    com = []
+    tokens = []
 
     for line in content.splitlines():
-        com.append(parse(line.lstrip().rstrip()))
+        line = line.lstrip().rstrip()
+        if len(line) > 0:
+            tokens.append(parse(line))
 
-    com = filter(None, com)
-    pprint(com)
+    tokens = filter(None, tokens)
+    code = generator(tokens)
+    # print code
+    open('test.c', 'w').write(code)
+    os.system('gcc test.c -o test')
+    print 'done'
 
 read()
